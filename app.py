f0db68240e5b55d3d11bd4089ed67a8aac97cb23
TSGreenwood <greenwood.ts@gmail.com>
2019-06-19 16:32:06 -0700

    change theme to template from bootstrapper

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ app = Flask(__name__, static_url_path="")
 @app.route('/')
 def index():
     """Return the main page."""
-    return render_template('index.html')
-
+    return render_template('theme.html')
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     """Return a random prediction."""
     data = request.json
     prediction = model.predict_proba([data['user_input']])
-    return jsonify({'probability': prediction[0][1]})
+    rounded_prediction = round(prediction[0][1], 2)
+    return jsonify({'probability': rounded_prediction})
 
